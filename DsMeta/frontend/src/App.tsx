5b9743b3ebddb13540ead220d13c3a9b05649bfa
Pedro Castro <pgkastro@gmail.com>
2022-09-06 14:44:04 -0300

    Other components

--- DsMeta/frontend/src/App.tsx
+++ DsMeta/frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import NotificationButton from './assets/components/NotificationButton'
 import Header from './assets/components/Header'
+import Card from './assets/components/Card'
 
 function App() {
   return (
     <>
       <Header />
-      <NotificationButton />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <Card />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
